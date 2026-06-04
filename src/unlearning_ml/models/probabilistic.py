3dcbf3dbb7ace47f48f19c9b3746b8ff672294e2
nakshatra k <95270855+Zburgers@users.noreply.github.com>
2026-06-04 20:36:59 +0530

    Add probabilistic placeholder module

---
+++ src/unlearning_ml/models/probabilistic.py
@@ -0,0 +1,9 @@
+"""Probabilistic model helpers.
+
+Add reusable Bayesian, Naive Bayes, GMM, and HMM utilities here when those phases begin.
+"""
+
+from __future__ import annotations
+
+
+PROBABILISTIC_HELPERS_NOT_INITIALIZED = "Probabilistic helpers will be added in later phases."
